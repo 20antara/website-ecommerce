cd62e5c19b98b97e7b7c0262f5d6f5dd4c81c8c8
20antara <antara20_ug@cse.nits.ac.in>
2025-07-17 00:56:59 +0530

    Assets created

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const orders = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Orders
